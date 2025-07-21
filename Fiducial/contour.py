4e3bfe6252e955bc842b1afa3368183f46f57a83
fidelisprasetyo <fidelisprasetyo@gmail.com>
2025-07-21 14:28:09 -0700

    update progress

--- Fiducial/contour.py
+++ Fiducial/contour.py
@@ -2,106 +2,132 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 from bosdyn.client.image import ImageClient
+from collections import defaultdict
 
-def detect_contour(frame):
-    AREA_MIN = 1000
+AREA_MIN = 200
 
-    gray_frame_overlay = frame.copy()
+def prepare_image_response(image_responses):
+    dtype = np.uint8
+    frame = np.frombuffer(image_responses[0].shot.image.data, dtype=dtype)
+    frame = cv2.imdecode(frame, -1)
+
+    if image_responses[0].source.name[0:5] == "front":
+        frame = cv2.rotate(frame, cv2.ROTATE_90_CLOCKWISE)
+
+    elif image_responses[0].source.name[0:5] == "right":
+        frame = cv2.rotate(frame, cv2.ROTATE_180)
+
+    return frame
+
+def convert_to_bin(frame):
+
     # Gaussian blur
     blurred_frame = cv2.GaussianBlur(frame, (5, 5), 0)
-    # Histogram Equalization
-    chale = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-    eq_frame = chale.apply(blurred_frame)
+    # # Histogram Equalization -> too heavy
+    # chale = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
+    # eq_frame = chale.apply(blurred_frame)
     # Otsu's thresholding
-    otsu_t, bin_frame = cv2.threshold(eq_frame, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
+    otsu_t, bin_frame = cv2.threshold(blurred_frame, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
     # Morphological closing
     kernel = np.ones((5, 5), np.uint8)
     closed_frame = cv2.morphologyEx(bin_frame, cv2.MORPH_CLOSE, kernel)
+
+    return closed_frame
+
+def compute_center(contour):
+    M = cv2.moments(contour)
+    if M["m00"] != 0:
+        cX = int(M["m10"] / M["m00"])
+        cY = int(M["m01"] / M["m00"])
+    return cX, cY
+
+# won't be used
+def draw_board(frame, grids):
+    for contour in grids:
+        cv2.drawContours(frame, [contour], -1, (125, 125, 125), 2)
+
+# grid tuple format: ((x,y), area)
+# grid map:
+# | (2,0) | (2,1) | (2,2)
+# | (1,0) | (1,1) | (1,2)
+# | (0,0) | (0,1) | (0,2)
+
+def get_grid(grids, row, col):
+
+    if row == 0 and len(grids) < 3:
+        print(f"[Warning] Not enough grids for bottom row (found {len(grids)}).")
+        return None
+    elif row == 1 and len(grids) < 6:
+        print(f"[Warning] Not enough grids for middle row (found {len(grids)}).")
+        return None
+    elif row == 2 and len(grids) < 9:
+        print(f"[Warning] Not enough grids for top row (found {len(grids)}).")
+        return None
+
+    y_sorted = sorted(grids, key=lambda x: x[0][1], reverse=True)
+
+    try:
+        x_sorted_bottom = sorted(y_sorted[:3], key=lambda x: x[0][0])
+        x_sorted_middle = sorted(y_sorted[3:6], key=lambda x: x[0][0])
+        x_sorted_top = sorted(y_sorted[6:9], key=lambda x: x[0][0])
+        if row == 0:
+            return x_sorted_bottom[col]
+        if row == 1:
+            return x_sorted_middle[col]
+        if row == 2:
+            return x_sorted_top[col]
+
+    except IndexError:
+        print(f"[Error] Column index {col} out of range for row {row}.")
+        return None
+
+
+def draw_board_centers(frame, grids):
+    for grid in grids:
+        point = grid[0]
+        cv2.putText(frame, ".", point, cv2.FONT_HERSHEY_SIMPLEX, 0.6, (255, 0, 0), 2)
+
+def get_board_grids(frame):
+
+    rectangles = defaultdict(list)
+    grids = []
+
+    bin_frame = convert_to_bin(frame)
+
     # Find contours
-    contours, hierarchy = cv2.findContours(closed_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    rectangle_count = 0
+    contours, hierarchy = cv2.findContours(bin_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+
     if hierarchy is not None:
         for idx, cnt in enumerate(contours):
-            approx = cv2.approxPolyDP(cnt, 0.02 * cv2.arcLength(cnt, True), True)
-            if len(approx) == 4 and cv2.isContourConvex(approx):
-                area = cv2.contourArea(approx)
-                if area > AREA_MIN and hierarchy[0][idx][3] != -1:
-                    cv2.drawContours(gray_frame_overlay, [approx], -1, (255, 0, 0), 2)
-                    M = cv2.moments(approx)
-                    if M["m00"] != 0:
-                        cX = int(M["m10"] / M["m00"])
-                        cY = int(M["m01"] / M["m00"])
-                        cv2.putText(gray_frame_overlay, f"{rectangle_count+1}", (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 255), 2)
-                    rectangle_count += 1
-    
-    # Show result
-    cv2.imshow("Tictacspot_gray", gray_frame_overlay)
-    cv2.imshow("Tictacspot_bin", closed_frame)
+            parent = hierarchy[0][idx][3]
+            if parent == -1:
+                continue
+            cnt_approx = cv2.approxPolyDP(cnt, 0.02 * cv2.arcLength(cnt, True), True)
 
-def track(image_client: ImageClient, image_source = "right_fisheye_image"):
+            # filter out non-rectangles
+            if len(cnt_approx) == 4 and cv2.isContourConvex(cnt_approx):
+                area = cv2.contourArea(cnt_approx)
+                if area > AREA_MIN:
+                    rectangles[parent].append(cnt_approx)
 
-    AREA_MAX = 1000
+    for parent, children in rectangles.items():
+        if len(children) >= 3:
+            for child in children:
+                grid_tuple = (compute_center(child), cv2.contourArea(child))
+                grids.append(grid_tuple)
 
-    while True:
-        image_responses = image_client.get_image_from_sources([image_source])
+    return grids
 
-        dtype = np.uint8
+def is_x_aligned(grid, threshold, x):
+    x_grid = grid[0][0]
+    return abs(x_grid - x) <= threshold
 
-        gray_frame = np.frombuffer(image_responses[0].shot.image.data, dtype=dtype)
-        gray_frame = cv2.imdecode(gray_frame, -1)
+def is_y_aligned(grid, threshold, y):
+    y_grid = grid[0][1]
+    return abs(y_grid - y) <= threshold
 
-        if image_responses[0].source.name[0:5] == "front":
-            gray_frame = cv2.rotate(gray_frame, cv2.ROTATE_90_CLOCKWISE)
-
-        elif image_responses[0].source.name[0:5] == "right":
-            gray_frame = cv2.rotate(gray_frame, cv2.ROTATE_180)
-
-        gray_frame = cv2.resize(gray_frame, (640, 480))
-
-        gray_frame_overlay = gray_frame.copy()
-
-        # Gaussian blur
-        blurred_frame = cv2.GaussianBlur(gray_frame, (5, 5), 0)
-
-        # Histogram equalization
-        chale = cv2.createCLAHE(clipLimit=2.0, tileGridSize=(8,8))
-        eq_frame = chale.apply(blurred_frame)
-
-        # Otsu thresholding / Adaptive thresholding
-        otsu_t, bin_frame = cv2.threshold(eq_frame, 0, 255, cv2.THRESH_BINARY_INV + cv2.THRESH_OTSU)
-
-        # Morphological closing
-        kernel = np.ones((5, 5), np.uint8)
-        closed_frame = cv2.morphologyEx(bin_frame, cv2.MORPH_CLOSE, kernel)
-
-        # Find contours with hierarchy
-        contours, hierarchy = cv2.findContours(closed_frame, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-
-        # Draw and label detected rectangles
-        rectangle_count = 0
-        if hierarchy is not None:
-            for idx, cnt in enumerate(contours):
-                approx = cv2.approxPolyDP(cnt, 0.02 * cv2.arcLength(cnt, True), True)
-                if len(approx) == 4 and cv2.isContourConvex(approx):
-                    area = cv2.contourArea(approx)
-                    if area > AREA_MAX and hierarchy[0][idx][3] != -1:
-                        cv2.drawContours(gray_frame_overlay, [approx], -1, (255, 0, 0), 2)
-                        M = cv2.moments(approx)
-                        if M["m00"] != 0:
-                            cX = int(M["m10"] / M["m00"])
-                            cY = int(M["m01"] / M["m00"])
-                            cv2.putText(gray_frame_overlay, f"{area}", (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 255, 255), 2)
-                        rectangle_count += 1
-
-        # Show result
-        cv2.imshow("Tictacspot", gray_frame_overlay)
-        cv2.imshow("Tictacspot_bin", closed_frame)
-
-        # Press 'q' to exit
-        if cv2.waitKey(1) & 0xFF == ord('q'):
-            break
-
-    cv2.destroyAllWindows()
+def area_diff(grid, target_area):
+    return abs(grid[1] - target_area)
 
 def save_debug_image(image, title="Image", filename="output.png", cmap='gray'):
     plt.figure()
@@ -120,19 +146,3 @@ def save_histogram(image, title, filename, otsu_thresh):
     plt.ylabel('# of pixels')
     plt.axvline(x=otsu_thresh, color='red', linestyle='--', label=f"Otsu: {int(otsu_thresh)}")
     plt.savefig(filename)
-
-
-# cap = cv2.VideoCapture(0)
-# if not cap.isOpened():
-#     print("Cannot open Camera")
-#     exit()
-
-# while True:
-#     ret, frame = cap.read()
-#     frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-#     detect_contour(frame)
-
-#         # Press 'q' to exit
-#     if cv2.waitKey(1) & 0xFF == ord('q'):
-#         break
-
--- Fiducial/test.py
+++ Fiducial/test.py
@@ -2,26 +2,81 @@ import cv2
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
+import time
+import math
 
 import bosdyn.client
 import bosdyn.client.lease
 import bosdyn.client.util
 import bosdyn.geometry
-from bosdyn.api import trajectory_pb2
+from bosdyn.api import trajectory_pb2, geometry_pb2
 from bosdyn.api.spot import robot_command_pb2 as spot_command_pb2
-from bosdyn.client import math_helpers
-from bosdyn.client.frame_helpers import GRAV_ALIGNED_BODY_FRAME_NAME, ODOM_FRAME_NAME, get_a_tform_b
-from bosdyn.client.image import ImageClient
-from bosdyn.client.robot_command import RobotCommandBuilder, RobotCommandClient, blocking_stand
+from bosdyn.client.frame_helpers import BODY_FRAME_NAME, ODOM_FRAME_NAME, get_a_tform_b, get_vision_tform_body
+from bosdyn.client.math_helpers import Quat
+from bosdyn.client.image import ImageClient, pixel_to_camera_space
+from bosdyn.client.robot_command import RobotCommandBuilder, RobotCommandClient, blocking_stand, block_until_arm_arrives
 from bosdyn.client.robot_state import RobotStateClient
 from bosdyn.util import seconds_to_duration
 from bosdyn.api import image_pb2
 
-from contour import track
+from contour import *
 
 from PIL import Image
 import io
 
+#### Some configs (px) #####
+TOLERANCE = 100
+TARGET_AREA = 2500
+############################
+
+
+def robot_move(command_client, _time = 0.5, x = 0.0, y = 0.0, rot = 0.0):
+    cmd = RobotCommandBuilder.synchro_velocity_command(v_x = x, v_y = y, v_rot = rot)
+    command_client.robot_command(command = cmd, end_time_secs = time.time() + _time)
+    time.sleep(1)
+
+def robot_roll(command_client, _roll):
+    footprint_R_body = bosdyn.geometry.EulerZXY(yaw=0.0, roll=_roll, pitch=0.0)
+    cmd = RobotCommandBuilder.synchro_stand_command(footprint_R_body=footprint_R_body)
+    command_client.robot_command(command = cmd)
+    time.sleep(1)
+
+
+def get_world_coordinates(grid, image_responses, robot_state_client):
+
+    grid_x = grid[0][0]
+    grid_y = grid[0][1]
+    grid_depth = get_depth(image_responses, grid_x, grid_y)
+
+    if grid_depth == 0:
+        print("we got zero depth!!")
+        return None
+
+    cam_coords = pixel_to_camera_space(image_responses[1], grid_x, grid_y, depth=grid_depth)
+    approach_dir = np.array(cam_coords) / np.linalg.norm(cam_coords)
+    offset_cam_coords = cam_coords - approach_dir * 0.30
+    T_world_cam = get_a_tform_b(image_responses[1].shot.transforms_snapshot, "odom", "left_fisheye")
+    world_point = T_world_cam.transform_point(*offset_cam_coords)
+
+    robot_state = robot_state_client.get_robot_state()
+    robot_rt_world = get_vision_tform_body(robot_state.kinematic_state.transforms_snapshot)
+
+    target_vector = np.array([world_point[0] - robot_rt_world.x, world_point[1] - robot_rt_world.y,0])
+    target_direction = target_vector / np.linalg.norm(target_vector)
+    zhat = [0.0, 0.0, 1.0]
+    yhat = np.cross(zhat, target_direction)
+    mat = np.array([target_direction, yhat, zhat]).T
+    angle_desired = Quat.from_matrix(mat).to_yaw()
+
+    return (world_point, angle_desired)
+
+def get_depth(image_responses, x, y):
+    cv_depth = np.frombuffer(image_responses[0].shot.image.data, dtype=np.uint16)
+    cv_depth = cv_depth.reshape(image_responses[0].shot.image.rows, image_responses[0].shot.image.cols)
+
+    depth = cv_depth[y, x]/1000
+
+    return depth
 
 def main():
     parser = argparse.ArgumentParser()
@@ -35,12 +90,154 @@ def main():
     bosdyn.client.util.authenticate(robot)
     robot.time_sync.wait_for_sync()
     
+    ### main
+
     assert not robot.is_estopped()
     robot_state_client = robot.ensure_client(RobotStateClient.default_service_name)
     lease_client = robot.ensure_client(bosdyn.client.lease.LeaseClient.default_service_name)
-    image_client = robot.ensure_client(ImageClient.default_service_name)
+
+
+    with bosdyn.client.lease.LeaseKeepAlive(lease_client, must_acquire=True, return_at_exit=True):
+        robot.logger.info('Powering on robot... This may take several seconds.')
+        robot.power_on(timeout_sec=20)
+        assert robot.is_powered_on(), 'Robot power on failed.'
+        robot.logger.info('Robot powered on.')
+
+        image_client = robot.ensure_client(ImageClient.default_service_name)
+
+        # stand up!
+        command_client = robot.ensure_client(RobotCommandClient.default_service_name)
+        blocking_stand(command_client)
+        time.sleep(1)
+
+        roll = 0.0
+        board_found = False
+        while True:
+            image_responses = image_client.get_image_from_sources(["left_depth_in_visual_frame", "left_fisheye_image"])
+
+            cv_visual = cv2.imdecode(np.frombuffer(image_responses[1].shot.image.data, dtype=np.uint8), -1)
+
+            img_res = (image_responses[1].shot.image.cols, image_responses[1].shot.image.rows)
+            gray_frame = cv_visual
+            board_grids = get_board_grids(gray_frame)
+
+            draw_board_centers(gray_frame, board_grids)
+            cv2.imshow("Tictacspot", cv2.resize(gray_frame, (640, 480)))
+
+            grid_count = len(board_grids)
+            if not board_found:
+                if grid_count == 9:
+                    grid_11 = get_grid(board_grids, 1, 1)
+                    if is_x_aligned(grid_11, TOLERANCE, img_res[0]/2):
+                        if is_y_aligned(grid_11, TOLERANCE, img_res[1]/2 - 40):
+                            print(f"[Detected grids: {grid_count}], Board is found and aligned!")
+                            board_found = True
+                        else:
+                            print(f"[Detected grids: {grid_count}], Board found: aligning...")
+                            if roll == 0.4:
+                                roll = 0.0
+                            else:
+                                roll = roll + 0.1
+                            robot_roll(command_client, roll)
+                    else:
+                        print(f"[Detected grids: {grid_count}], Board found: aligning...")
+                        robot_move(command_client, rot = 0.5)
+                elif grid_count >= 3 and grid_count < 9:
+                    print(f"[Detected grids: {grid_count}], Board is partially found!")
+                    grid_01 = get_grid(board_grids, 0, 1)
+
+                    if is_x_aligned(grid_01, TOLERANCE, img_res[0]/2):
+                        if roll == 0.4:
+                            roll = 0.0
+                        else:    
+                            roll = roll + 0.1
+                        robot_roll(command_client, roll)
+                    else:
+                        print(f"[Detected grids: {grid_count}], Partial found: aligning...")
+                        robot_move(command_client, rot = 0.5)
+                else:
+                    print("No board is found, try rotate")
+                    robot_move(command_client, rot = 0.5)
+
+            
+            if board_found:
+                grids = [get_grid(board_grids, 0,0), get_grid(board_grids, 0,1), get_grid(board_grids, 0,2)]
+                world_points = []
+                for grid in grids:
+                    world_points.append(get_world_coordinates(grid, image_responses, robot_state_client))
+
+                #blocking_stand(command_client)
+                for world_point in world_points:
+                    # gaze_command = RobotCommandBuilder.arm_gaze_command(x = world_point[0][0], y = world_point[0][1], z = world_point[0][2], frame_name="odom")
+                    arm_command = RobotCommandBuilder.arm_pose_command(
+                        frame_name="odom",
+                        x = world_point[0][0],
+                        y = world_point[0][1],
+                        z = world_point[0][2],
+                        qw = np.cos(world_point[1] / 2),
+                        qx = 0.0,
+                        qy = 0.0,
+                        qz = np.sin(world_point[1] / 2),
+                        seconds=2.0
+                    )
+                    follow_arm_command = RobotCommandBuilder.follow_arm_command()
+                    # gripper_command = RobotCommandBuilder.claw_gripper_open_command()
+                    synchro_command = RobotCommandBuilder.build_synchro_command(arm_command, follow_arm_command)
+                    command_client.robot_command(synchro_command)
+                    time.sleep(10)
+
+                # grid_01_area = get_grid(board_grids, 0, 1)[1]
+                # area_diff = TARGET_AREA - grid_01_area
+                # if area_diff > 300:
+                #     print(f"Area difference: {area_diff}, moving closer...")
+                #     robot_move(command_client, y=-0.2)
+                # elif area_diff < -300:
+                #     robot_move(command_client, y=0.2)
+                #     print(f"Area difference: {area_diff}, moving farther...")
+                # else:
+                #     if grid_count == 9:
+                #         print("PERFECT")
+                #     else:
+                #         board_found = False
+
+            #Press 'q' to exit
+            if cv2.waitKey(1) & 0xFF == ord('q'):
+                break
+        cv2.destroyAllWindows()
     
-    track(image_client)
+
+    # ### for testing purposes
+    # image_client = robot.ensure_client(ImageClient.default_service_name)
+    # test_list = []
+    # while True:
+    #     image_responses = image_client.get_image_from_sources(["left_depth_in_visual_frame", "left_fisheye_image"])
+
+    #     cv_depth = np.frombuffer(image_responses[0].shot.image.data, dtype=np.uint16)
+    #     cv_depth = cv_depth.reshape(image_responses[0].shot.image.rows, image_responses[0].shot.image.cols)
+    #     cv_visual = cv2.imdecode(np.frombuffer(image_responses[1].shot.image.data, dtype=np.uint8), -1)
+    #     visual_rgb = cv_visual if len(cv_visual.shape) == 3 else cv2.cvtColor(
+    #     cv_visual, cv2.COLOR_GRAY2RGB)
+    #     min_val = np.min(cv_depth)
+    #     max_val = np.max(cv_depth)
+    #     depth_range = max_val - min_val
+    #     depth8 = (255.0 / depth_range * (cv_depth - min_val)).astype('uint8')
+    #     depth8_rgb = cv2.cvtColor(depth8, cv2.COLOR_GRAY2RGB)
+    #     depth_color = cv2.applyColorMap(depth8_rgb, cv2.COLORMAP_JET)
+    #     out = cv2.addWeighted(visual_rgb, 0.5, depth_color, 0.5, 0)
+
+    #     # board_girds = get_board_grids(cv_visual)
+    #     # grid_11 = get_grid(board_girds, 1, 1)
+
+    #     # depth_mm = cv_depth[grid_11[0][1], grid_11[0][0]]
+    #     # depth_m = depth_mm / 1000.0
+
+
+    #     # draw_board_centers(cv_visual, board_girds)
+    #     cv2.imshow("Tictacspot", out)
+    
+    #     if cv2.waitKey(1) & 0xFF == ord('q'):
+    #         break
+    # cv2.destroyAllWindows()
 
 
 if __name__ == '__main__':
